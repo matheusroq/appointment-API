174a4f71f370f3b522dd35f181a1d7ca130c3a3c
Matheus Roque <78318587+matheusroq@users.noreply.github.com>
2021-05-07 12:13:14 -0300

    authentication

--- routes.js
+++ routes.js
@@ -9,15 +9,15 @@ import loginRequired from './src/middlewares/loginRequired';
 
 //appointments
 router.get('/appointments', loginRequired, appointmentController.index);
-router.get('/appointment/:id',authentication, appointmentController.show);
-router.post('/appointment',authentication, appointmentController.store);
-router.put('/appointment/:id',authentication, appointmentController.update);
-router.delete('/appointment/:id',authentication, appointmentController.delete);
+router.get('/appointment/:id',loginRequired, appointmentController.show);
+router.post('/appointment',loginRequired, appointmentController.store);
+router.put('/appointment/:id',loginRequired, appointmentController.update);
+router.delete('/appointment/:id',loginRequired, appointmentController.delete);
 
 //users
 router.post('/user', userController.store);
-router.put('/user/:id',authentication, userController.update);
-router.delete('/user/:id',authentication, userController.delete);
+router.put('/user/:id',loginRequired, userController.update);
+router.delete('/user/:id',loginRequired, userController.delete);
 
 //token
 router.post('/token', tokenController.createToken);
