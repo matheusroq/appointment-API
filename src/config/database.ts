2f39a01b2d288c73f2be97396bc74fe634c7baad
Matheus Roque <matheusfariasroq@gmail.com>
2021-11-12 22:36:22 -0300

    add sequelize connection

---
+++ src/config/database.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize'
+
+const dbName = process.env.DB_DATABASE as string;
+const dbUser = process.env.DB_USER as string;
+const dbHost = process.env.DB_HOST as string;
+const dbPassword = process.env.DB_PASSWORD as string;
+
+const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: 'mysql'
+})
+
+export default sequelizeConnection;
